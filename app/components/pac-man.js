ca894614f05a09ebbf8eb01595e08247ad2a69bb
Alexander Beloborodov <astrophytum1411@gmail.com>
2016-08-09 15:08:30 +0000

    start

---
+++ app/components/pac-man.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
+
+export default Ember.Component.extend(KeyboardShortcuts, {
+    
+    keyboardShortcuts: {
+        up: function() { console.log('up'); },
+        down: function() { console.log('down'); },
+        left: function() { console.log('left'); },
+        right: function() { console.log('right'); }
+    },
+    
+    didInsertElement: function() {
+        this.drawCirle();
+    },
+    
+    drawCircle: function() {
+        let canvas = document.getElementById('myCanvas');
+        let ctx = canvas.getContext("2d");
+        let x = 50;
+        let y = 100;
+        let radius = 20;
+        
+        ctx.fillStyle = '#000';
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+    }
+    
+});
